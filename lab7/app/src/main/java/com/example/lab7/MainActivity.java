b50f7f200098c9880e8ad28113521e743b1eab6c
vard0ni <ip_goose@mail.ru>
2023-12-10 23:46:40 +0300

    lab7

---
+++ lab7/app/src/main/java/com/example/lab7/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.lab7;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    Button showDialogButton, finishActivityButton;
+    EditText dialogEditText;
+    AlertDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showDialogButton = findViewById(R.id.show_dialog_button);
+        finishActivityButton = findViewById(R.id.finish_activity_button);
+
+        showDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCustomDialog();
+            }
+        });
+
+        finishActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void showCustomDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
+        builder.setView(dialogView);
+
+        dialogEditText = dialogView.findViewById(R.id.dialog_edit_text);
+
+        builder.setPositiveButton("Show text", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String text = dialogEditText.getText().toString().trim();
+                if (!text.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Text from dialog: " + text, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Enter text", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog = builder.create();
+        dialog.show();
+    }
+}
